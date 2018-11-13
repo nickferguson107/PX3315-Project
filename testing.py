46c3e4dbfdd8533eb4c63f71247df3d0226fe1cf
Nick <nickferguson107@icloud.com>
2018-11-13 15:36:12 +0000

    Added functions to RandomGraph class.

--- testing.py
+++ testing.py
@@ -1,17 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 class RandomGraph():
 
     def __init__(self, nodes):
         self.node_number = nodes
-        self.node_dict = {i: '' for i in range(nodes)}
     
-    def poisson_distribution(self):
-        p = np.random.poisson(1, size=self.node_number)
-        return p
+    def poisson_distribution(self, mean_degree):
+        p = np.random.poisson(mean_degree, size=self.node_number)
+        return p+1
+    
+    def adjacency_matrix(self, nodes, p):
+        am = np.zeros((nodes, nodes))
+        node_list = [i for i in list(range(nodes))]
+        for i in range(nodes):
+            nodes_to_connect = random.sample(node_list, p[i])
+            print(nodes_to_connect)
+            for j in nodes_to_connect:
+                am[i,j], am[j,i] = int(1)
+        np.savetxt("am.csv", am, fmt='%i', delimiter='\t')
+        return am
 
-nodes = 100
+    def connected_nodes(self, am):
+        node_dict = {i : [] for i in range(len(am[0:,0]))}
+        for i in range(nodes):
+            for j in range(nodes):
+                if am[i,j] == 1:
+                    node_dict[i].append(j)
+        return node_dict
+
+nodes = 10
+mean_deg = 1
 a = RandomGraph(nodes)
-b = a.poisson_distribution()
+b = a.poisson_distribution(mean_deg)
+c = a.adjacency_matrix(nodes, b)
+d = a.connected_nodes(c)
 print(b)
+print(c)
+print(d)
+mean_test = [len(i) for i in d.values()]
+print(np.mean(mean_test))
