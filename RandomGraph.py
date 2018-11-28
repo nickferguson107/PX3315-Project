b05a2b1114818c26802bc1ce2037d831d8d6c0c3
Nick <nickferguson107@icloud.com>
2018-11-28 23:57:09 +0000

    Improved randomness of generation

--- RandomGraph.py
+++ RandomGraph.py
@@ -7,33 +7,62 @@ class RandomGraph():
         self.node_number = nodes
     
     def poisson_distribution(self, k):
-        p = np.random.poisson(k, size=self.node_number)
+        while(1):
+            p = np.random.poisson(k, size=self.node_number)
+            if np.sum(p+1)%2 == 0:
+                break
         return p+1
     
     def adjacency_matrix(self, nodes, p):
+        for i in p:
+            if i < nodes:
+                break
+            return ValueError('Degree higher than number of nodes.')     
         am = np.zeros((nodes, nodes))    # Create blank matrix
         nodes = list(range(nodes))    # List of node numbers (0, 1, ..., max)
         node_pairs = []    # Will become list of node pairs
-        for i in range(len(p)):    # For each value of Poisson sample...
-            for j in range(p[i]):    # Take that value, and for that many times...
-                origin_node = nodes[i]    # Get a random node
+        while(np.sum(p)>0):
+            for i in range(len(p)):
+                #print('Current degree distribution: {}'.format(p))
+                if p[i] == 0:
+                    continue
+                print('Starting node: {}.'.format(i))
+                origin_node = nodes[i]
                 while(1):
-                    chosen_node = random.choice(nodes)    # Choose another node at random
-                    if chosen_node != origin_node:    # Check they're not the same
-                        break               
-                node_pairs.append([j, chosen_node])    # Connect nodes, add to list of node pairs
-                am[origin_node, chosen_node] = int(1)    # Add connection to adjaceny matrix
-                am[chosen_node, origin_node] = int(1)
+                    connection_node = random.choice(nodes)
+                    print('Attempting to connect node {} to node {}...'.format(origin_node, connection_node))
+                    if p[connection_node] == 0:
+                        print('Connection node invalid: node already full.')
+                        continue
+                    if connection_node != origin_node:
+                        break
+                    print('Connection node invalid: can\'t connect node to itself.')
+                node_pairs.append([origin_node, connection_node])    # Connect nodes, add to list of node pairs
+                am[origin_node, connection_node] += int(1)    # Add connection to adjaceny matrix
+                am[connection_node, origin_node] += int(1)
+                print('Connection {} to {} made successfully.'.format(origin_node, connection_node))
+                #print(am)
+                p[i] = p[i]-1
+                p[connection_node] = p[connection_node]-1
+                #print('Updated degree distribution: {}'.format(p))
+                if np.sum(p) == 0:
+                    print('All nodes filled.')
+                    break
         np.savetxt("am.csv", am, fmt='%i', delimiter='\t')
         return am
 
     def check_symmetry(self, nodes, am):
-        for i in range(nodes):
-            for j in range(nodes):
-                if am[i,j] != am[j,i]:
-                    return(print("Not symmetric"))
-                else:
-                    return(print("Symmetric"))
+        if type(am) == ValueError:
+            result = 'Invalid adjacency matrix.'
+            return result
+        else:
+            for i in range(nodes):
+                for j in range(nodes):
+                        if am[i,j] == am[j,i]:
+                            result = 'Adjacency matrix is symmetrical.'
+                        else:
+                            result = 'Adjacency matrix is not symmetrical.'
+            return result
 
 if __name__ == '__main__':
     print(1+1)
--- testing.py
+++ testing.py
@@ -1,10 +1,11 @@
 import RandomGraph
 
-nodes = 12
-mean_degree = 2
+nodes = int(input("Give number of nodes in graph: "))
+mean_degree = int(input("Give mean degree of nodes: "))
 
 graph = RandomGraph.RandomGraph(nodes)
-
 p = graph.poisson_distribution(mean_degree)
-
 matrix = graph.adjacency_matrix(nodes, p)
+sym = graph.check_symmetry(nodes, matrix)
+print(sym)
+input()
