a5b3c0417dcd32911455c3f1a9a07accaa9b9b22
Nick <nickferguson107@icloud.com>
2019-05-22 23:51:20 +0100

    Final commit

--- nx_comparison.py
+++ nx_comparison.py
@@ -1,4 +1,4 @@
-import networkx as nx
+from networkx import erdos_renyi_graph, connected_component_subgraphs
 import RandomGraph
 import general
 import numpy as np
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 
 def nx_giant_component(nodes, p):
 
-    G = nx.erdos_renyi_graph(nodes, p)
-    c = len(max(nx.connected_component_subgraphs(G), key=len))
+    G = erdos_renyi_graph(nodes, p)
+    c = len(max(connected_component_subgraphs(G), key=len))
 
     return c
 
@@ -17,9 +17,9 @@ def nx_giant_component_range(nodes, p_range):
 
     return sizes
 
-def plot_nx_components(nodes, filename="auto", k_min=0.1, k_max=5, num=1000, avg_width=5):
+def plot_nx_components(nodes, filename="auto", k_min=0, k_max=7.5, k_iters=1500, save=False):
 
-    k = np.linspace(k_min, k_max, num=num)
+    k = np.linspace(k_min, k_max, num=k_iters)
 
     if filename == "auto":
         filename = "n={}, k=({},{})".format(nodes, k_min, k_max)
@@ -27,12 +27,9 @@ def plot_nx_components(nodes, filename="auto", k_min=0.1, k_max=5, num=1000, avg
     sizes = nx_giant_component_range(nodes, k/nodes)
 
     y = [s/nodes for s in sizes]
-    avg = RandomGraph.moving_average(y, avg_width)
-    newk = np.linspace(k_min, k_max, num=len(avg))
     _, ax = plt.subplots()
 
     ax.plot(k, y, label="Component size")
-    ax.plot(newk, avg, label="Average", lw=2)
     ax.set(xlabel = r"$k$",
            ylabel = r"$\frac{N_{G}}{N}$",
            xlim = [k[0], k[-1]],
@@ -40,11 +37,31 @@ def plot_nx_components(nodes, filename="auto", k_min=0.1, k_max=5, num=1000, avg
     )  
     ax.grid()
     ax.legend(loc='best')
-    plt.savefig("Graphs/nx_{}.png".format(filename))
-    np.savetxt("Results/nx_{}.txt".format(filename),
-                np.transpose(np.vstack((k, y, avg))),
-                delimiter='\t')
+    if save:
+        plt.savefig("Graphs/nx_{}.png".format(filename))
+        np.savetxt("Results/nx_{}.txt".format(filename),
+                    np.transpose(np.vstack((k, y))),
+                    delimiter='\t')
     return k, y
+
+def multiplot(num, nodes, k_min, k_max):
+
+    all_arrays = np.empty((nodes, num))
+    filename = "n={}, k=({},{}), {} averaged".format(nodes, k_min, k_max, num)
+
+    for i in range(num):
+        print("Beginning iteration {}...".format(i))
+        _, all_arrays[:,i] = plot_nx_components(nodes, k_min = k_min, k_max = k_max, k_iters=nodes)
+        print("Finished iteration {}.".format(i))
+
+
+    averaged = np.mean(all_arrays, axis=1)
+
+    np.savetxt("Results/nx_{}.txt".format(filename), averaged, delimiter='\t')
+
+    return averaged
+
+
 
 
 a, b = plot_nx_components(1000, 'test')
--- testing.py
+++ testing.py
@@ -1,7 +1,13 @@
 import RandomGraph
-import CustomGraph
-import general
 import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
+'''
+own = RandomGraph.multiplot(100, 1000, 0, 7.5)
+nxc = nx_comparison.multiplot(100, 1000, 0, 7.5)
 
+dif = own - nxc
+'''
+
+d = np.loadtxt("Results\difference.txt", delimiter='\t')
+e = abs(d)
+a = [i for i in e if i > 0.01]
+print((1-(len(a)/1000))*100)
